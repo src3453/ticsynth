254525890e86194b64a5e9b56173e8e4fbee5c81
src3453 <info@example.com>
2023-10-29 13:53:11 +0900

    modified:   ticsynth.lua

--- ticsynth.lua
+++ ticsynth.lua
@@ -330,7 +330,7 @@ if ismelodic(ch) then
     --out = wfline(out,0,15-volume,0,15)
     --out = wfline(out,15-volume,31,15,0)
     --out = wfclip(wfline(out,0,15,15,volume),1,15)
-    local inst = insts[sfxnum[ch+1]] or insts[1]
+    local inst = insts[sfxnum[ch+1]+1] or insts[1]
     inst[11]=ch
     inst[12]=frame
     out = playFMinst(table.unpack(inst))
@@ -417,7 +417,7 @@ visualize() -- visualizer of sound registers
 end
 insts={
 				--define your FM instrumentals here...
-    {1,1,3,60,0,3,0.7,0,60,0}, --piano
+    {1,1,0,60,0,3,2,0,60,0}, --default
 }
 --insts[1]={1,1,3,30,1,3,0.7,0,30,1}
 --function OVR()vbank(1)ticsyn()for ch=0,3 do if peek4(2*0xff9c+ch*36+3)~=0 then poke4(2*0xff9c+ch*36+3,15)end end end --unused
